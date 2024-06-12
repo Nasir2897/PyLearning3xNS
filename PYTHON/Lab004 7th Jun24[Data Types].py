e0d714f736051982ce282fce344824275e255531
Nasir2897 <shaikhnasir371@gmail.com>
2024-06-12 13:44:44 +0000

    #Initial commit

---
+++ PYTHON/Lab004 7th Jun24[Data Types].py
@@ -0,0 +1,12 @@
+#taking input from user
+x = input("Enter your input here: ")
+print(x)
+
+#Data Types:
+#basic data types covered in previous course
+# Advanced data types: List,Tuple,Set,Dict
+
+# Integer as an input
+y = int(input("Enter the integer: "))
+
+print(5**2)
---
+++ PYTHON/Lab005 Loops.py
@@ -0,0 +1,5 @@
+# Learning Debugging
+
+for i in range(10):
+    print(i)
+
